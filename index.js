d4a428005927da112040f48c2ea445d323d4a1aa
Monika <91907563+mkford4@users.noreply.github.com>
2022-01-24 15:59:26 -0600

    Added table to documentation.html & method routes to index.js

--- index.js
+++ index.js
@@ -6,19 +6,55 @@ const app = express();
 
 //GET route at '/movies' that returns a JSON object with data on top movies
 app.get('/movies', (req, res) => {
-res.json(topMovies);
+  res.json(topMovies);
 });
 
 //GET route at '/' returning text response It's movie time!
 app.get('/', (req, res) => {
-res.send("It's movie time!");
+  res.send("It's movie time!");
 });
 
 //express.static to serve documentation.html from public folder
 app.use(express.static('public'));
 
+//code routes for all endpoints in documentation.html table
+app.get('/movies/:title', (req, res) => {
+  res.send('Successful GET request returning data on specified movie');
+});
+
+app.get('/movies/genre/:genreName', (req, res) => {
+  res.send('Successful GET request returning description of specified genre');
+});
+
+app.get('/movies/directors/:directorName', (req, res) => {
+  res.send('Successful GET request returning information of specified director');
+});
+
+app.post('/users/:newUser', (req, res) => {
+  //post request
+  res.send('Welcome! You are now registered as ' + username +'.');
+});
+
+app.put('/users/:username/favorites', (req, res) => {
+  res.send(movieTitle + 'has been added to your Favorites list.');
+});
+
+app.delete('/users/:username/favorites', (req, res) => {
+  res.send(movieTitle + 'has been deleted from your favorites.');
+});
+
+app.delete('/users/:username', (req, res) => {
+  res.send('Sad to see you go!');
+});
+
+
 //error-handling function to log all errors to the terminal
 app.use((err, req, res, next) => {
-console.error(err.stack);
-res.status(500).send("Something's wrong!"");
+  console.error(err.stack);
+  res.status(500).send("Something's wrong!");
+});
+
+//listen
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080');
 });
