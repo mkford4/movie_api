4589ea4860babf52264ae54f8653bd0979fde206
Monika <91907563+mkford4@users.noreply.github.com>
2022-01-24 16:11:19 -0600

    Added createServer function to index.js

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
 
-//require express, morgan middleware
+const http = require('http'),
+  url = require('url'),
+  fs = require('fs');
+
+http.createServer((request, response) => {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Hello Node!\n');
+/*
+  let addr = request.url,
+  q = url.parse(addr, true),
+  filePath = '';
+
+  if (q.pathname.includes('documentation')) {
+    filePath = (__dirname + '/documenation.html');
+    }else{
+    filePath = 'index.html';
+  };
+
+  fs.appendFile('log.txt', 'URL: ' + addr + '/nTimestamp: ' + newDate() + 'n/n', (err) => {
+    if (err) {
+      console.log(err);
+      }else{
+      console.log('Added to log.');
+      }
+  });
+
+  fs.readFile(filePath, (err,data) => {
+  if (err) {
+    throw err;
+    }
+  response.writeHead(200, {'Content-Type' : 'text/html'});
+  response.write(data);
+  response.end();
+  });
+*/
+
+//require Express, Morgan middleware
 const express = require('express');
   morgan = require('morgan');
 const app = express();
