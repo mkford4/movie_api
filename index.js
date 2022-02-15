4e291307ad97e8451f7aee8794bd6ca4cd15ad38
Monika <91907563+mkford4@users.noreply.github.com>
2022-02-15 13:49:55 -0600

    Added start to scripts in package.json; Updated listen() in index.js to include change in ports ability

--- index.js
+++ index.js
@@ -287,7 +287,8 @@ app.use((err, req, res, next) => {
   res.status(500).send("Something's wrong!");
 });
 
-//listen
-app.listen(8080, () => {
-  console.log('Your app is listening on port 8080');
+//listen with Heroku
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0', () => {
+  console.log('Listening on Port ' + port);
 });
